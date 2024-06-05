e2dd8323412580154262f7334647d085be5d7c73
Acted <t-913.saki@outlook.jp>
2024-06-05 16:59:44 +0900

    ikisugiiiiiiiii

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use std::env;
 use unicode_width::UnicodeWidthChar;
 fn main() {
-    let aa = std::fs::read_to_string("./AA.txt").unwrap();
+    let file = std::fs::read_to_string("./AA.txt");
+    let aa = match  file{
+        Ok (_) =>file.unwrap(),
+        Err(_) => "　　　　　　　　　　　　　 ,,,z=~'ﾞ'+''ｯ彡ｯ,､
+        　　　　　　　　　　　　,ｨ´ 　　　　　 \"':';:;ｯ;,
+        　　　　　　　　 ,　' ﾞ´`ﾞﾐﾞｯ,　　　　　　　 \"',`,
+        　　　　　　 ,／ 　　　 `､ﾞミ　　　　　　　　 ﾞ:;:,
+        　　　　　 /　　　　　 _ =ヾ､ﾞｼｼ=;,z,、　　　 ﾞ;ｼ::ﾐ
+        　　　　 /　　　　 ,ｒ,´　　 /　´`ヽ ゛ﾞ`　 　　,ﾞ彡:ﾐ
+        　　　 / 　　　, '-､_`ヽ_/,　　　　　　　 　 ﾐ;::彡;:
+        　　 ,'　　　,ｼ´｀｀ ヽ`i｀!　　　　　　　　 ,,彡;::ｼ:彡　
+        　　;i　　､（´ ￣`ヽ / '　　　　　　　　シ:ｼ;:ﾐ::ｼ\"
+        　ノ:!､　 ヽ｀`ｰ =;ｨ'　　　　　　　　,,ｼ:;彡;ｼﾞ
+        ´:::::.ヾ. 　　　￣´　　　　　　　　' `,ｼﾐﾞ
+        :::::::::::::.`:ヽ､_　　　　　　　...:;'＿,ソ'ﾞ''
+        ::::::::::::::::::::::::::｀:::::::::::::::-='".to_string(),
+    };
     let mut word:String ="".to_string();
     if env::args().len() == 1 {
         word = "イキスギィ".to_string();
